a1e2416dfd5d99ee18c90617619cfdad34fdf261
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-07-22 13:58:23 +0100

    Fisrt test in js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,22 @@
-let prevButton = document.getElementById("prev");
+let prevButton = document.getElementById("previus");
 let nextButton = document.getElementById("next");
 let conteiner = document.querySelector(".conteiner");
-let items = document.querySelectorAll(". list .item");
+let items = document.querySelectorAll(".list .item");
 let indicator = document.querySelector(".indicators");
 let dots = document.querySelectorAll(".indicators li");
+
+prevButton.addEventListener("click", function () {
+    /* let activeItem = document.querySelector(".list .item.active");
+    let activeDot = document.querySelector(".indicators li.active");
+    let prevItem = activeItem.previousElementSibling;
+    let prevDot = activeDot.previousElementSibling;
+
+    if (prevItem) {
+        activeItem.classList.remove("active");
+        prevItem.classList.add("active");
+
+        activeDot.classList.remove("active");
+        prevDot.classList.add("active");
+    }*/
+    console.log("Programdor");
+});
