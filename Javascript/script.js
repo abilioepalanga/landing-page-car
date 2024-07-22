0682f0671ca4be351aa68db92b03badccc612cad
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-07-22 22:49:46 +0100

    Try to fix the error function (JS)

--- Javascript/script.js
+++ Javascript/script.js
@@ -7,20 +7,27 @@ let dots = document.querySelectorAll(".indicators li");
 
 let active = 0;
 let fisrtItem = items[0];
-let lastItem = items.length - 1;
+let fisrtNumber = dots[0];
 prevButton.addEventListener("click", function () {
     let activeItem = document.querySelector(".list .item.active");
     let nextItem = activeItem.nextElementSibling;
+
     /*Numbers*/
-    let number = document.querySelector(".indicators .number li.active");
-    let nextNumber = number.nextElementSibling;
+    let activeNumber = document.querySelector(".indicators li.active2");
+    console.log("Active" + activeNumber.innerHTML);
+    /*let nextNumber = activeNumber.nextElementSibling;
+    console.log("NExt" + nextNumber);
 
     if (nextItem != null) {
         activeItem.classList.remove("active");
         nextItem.classList.add("active");
+
+        activeNumber.classList.remove("active2");
+        nextNumber.classList.add("active2");
     } else {
         nextItem = fisrtItem;
         activeItem.classList.remove("active");
         fisrtItem.classList.add("active");
     }
+    */
 });
