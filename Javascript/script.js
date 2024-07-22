99870e015159d785c4274170bbd6c7de8cac5219
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-07-22 13:49:58 +0100

    Created all necesary variables

---
+++ Javascript/script.js
@@ -0,0 +1,6 @@
+let prevButton = document.getElementById("prev");
+let nextButton = document.getElementById("next");
+let conteiner = document.querySelector(".conteiner");
+let items = document.querySelectorAll(". list .item");
+let indicator = document.querySelector(".indicators");
+let dots = document.querySelectorAll(".indicators li");
